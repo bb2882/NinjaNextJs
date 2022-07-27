9b54e6a116cd9d2122856e5fe6ebdc9ab2e39586
Azat <harutyunyanazat29@gmail.com>
2022-07-27 19:58:54 +0400

    auto redirect

--- pages/404.js
+++ pages/404.js
@@ -1,11 +1,23 @@
 import Link from 'next/link'
+import { useEffect } from 'react';
+import { useRouter } from 'next/router'
 
 const NotFound = () => {
+	const router = useRouter();
+
+	useEffect(() => {
+		setTimeout(() => {
+			// router.go(-1)
+			router.push('/')
+		}, 5000)
+	}, [])
+
 	return (
 		<div className="not-found">
 			<h1>Ooops...</h1>
 			<h2>That Page Cannot Be Found</h2>
 			<p>Go Back To The <Link href="/"><a>Homepage</a></Link></p>
+			<p className='alert'>You Will Be Redirected To The Homepage after 5 sec...</p>
 		</div>
 	);
 }
